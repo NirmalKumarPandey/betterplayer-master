6e9d43c10adc216686d0b5c7ea761fa90cf4ee47
Naveen Jindal <jindalnaveen4@gmail.com>
2022-01-08 18:44:19 +0530

    Video player with speed and resolution

---
+++ example/lib/constants.dart
@@ -0,0 +1,25 @@
+class Constants {
+
+
+
+  static const String fileTestVideoEncryptUrl = "testvideo_encrypt.mp4";
+
+
+  static const Map<String, String> exampleResolutionsUrls = {
+    "LOW":
+        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
+    "MEDIUM":
+        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4",
+    "LARGE":
+        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4",
+    "EXTRA_LARGE":
+        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4"
+  };
+
+  static const String elephantDreamVideoUrl =
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
+
+
+  static String logo = "logo.png";
+
+}
